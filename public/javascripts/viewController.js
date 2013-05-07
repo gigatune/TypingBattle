8863fea786227e6109a09b6490fd0a9f5c5c2990
gigatune <gigatune@github.com>
2013-05-07 19:06:42 +0900

    scoreブロックを削除

--- public/javascripts/viewController.js
+++ public/javascripts/viewController.js
@@ -1,12 +1,9 @@
 function viewController( ){
 
-    this.setScore_a = function( score ){
-        $('#score_a').html( score );
-    };
-
     this.setWord = function( team, word, index ){
         $('#word_word_' + team ).html( word.shownWord );
         $('#word_ruby_' + team ).html( word.ruby.substr( index, word.length ) );
     };
+
 
 };
