7ae57a2e27f0c489f9d5432b2dac354a65da3eb4
gigatune <gigatune@github.com>
2013-05-07 17:40:49 +0900

    チームを指定して回答できるように修正

--- public/javascripts/questionController.js
+++ public/javascripts/questionController.js
@@ -8,7 +8,7 @@ function questionController( med ){
 questionController.prototype.judge = function( keycode ){
     var char = String.fromCharCode( keycode ).toLowerCase();
     if( this.word.judge( char ) == true ){
-	this.socket.emit('answer', ( 'a,' + this.word.answerIndex ) );
+	this.socket.emit('answer', ( this.team + ',' + this.word.answerIndex ) );
     };
 };
 
--- public/javascripts/viewController.js
+++ public/javascripts/viewController.js
@@ -5,7 +5,6 @@ function viewController( ){
     };
 
     this.setWord = function( team, word, index ){
-
         $('#word_word_' + team ).html( word.shownWord );
         $('#word_ruby_' + team ).html( word.ruby.substr( index, word.length ) );
     };
