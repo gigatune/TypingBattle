5233279740db46e98340deb84a12052dde66880a
gigatune <gigatune@github.com>
2013-05-08 11:25:02 +0900

    キー入力をした時からタイマーを開始。10分たつと自動的にストップ。

--- public/javascripts/PlayingTimer.js
+++ public/javascripts/PlayingTimer.js
@@ -1,15 +1,20 @@
 function PlayingTimer(){
-    var timerId ;
+    var timerId;
 
     this.start = function(){
 	var passedTime = 0;
-	var interval = 200;  // ms
+	var interval = 1000;  // ms
+	var maxTime = 1000 * 60 * 10 // 10sec
 
 	var repeatFunc = function(){
 	    passedTime += interval;
 	    $('#timerDiv').html( passedTime );
+
+	    if( passedTime > maxTime ){
+		window.clearInterval( timerId );
+	    };
 	};
-	timerId = window.setInterval( repeatFunc, 1000 );
+	timerId = window.setInterval( repeatFunc, interval );
     };
 
     this.end = function(){
